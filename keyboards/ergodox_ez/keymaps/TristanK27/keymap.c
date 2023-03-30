d2375328dfd71efdf690e224ee5222c84c970455
TristanK27 <xb9j5e2ff@mozmail.com>
2023-03-30 19:58:42 +0200

    Added 0 numpad key and hr format.

--- keyboards/ergodox_ez/keymaps/TristanK27/keymap.c
+++ keyboards/ergodox_ez/keymaps/TristanK27/keymap.c
@@ -144,13 +144,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /*
-
+ * Keymap 4: [NUMB] Number pad layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |           | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |  TRNS  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |           | TRNS | TRNS |  7   |  8   |  9   | TRNS | TRNS   |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS |------|           |------| TRNS |  4   |  5   |  6   | TRNS | TRNS   |
+ * |--------+------+------+------+------+------| TRNS |           | TRNS |------+------+------+------+------+--------|
+ * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS |      |           |      |  0   |  1   |  2   |  3   | TRNS | TRNS   |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | TRNS | TRNS | TRNS | TRNS | TRNS |                                       | TRNS | TRNS | TRNS | TRNS | TRNS |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | TRNS | TRNS |       | TRNS | TRNS |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | TRNS |       | TRNS |        |      |
+ *                                 | TRNS | TRNS |------|       |------| TRNS  | TRNS |
+ *                                 |      |      | TRNS |       | TRNS |        |      |
+ *                                 `--------------------'       `----------------------'
 */
 [NUMB] = LAYOUT_ergodox_pretty(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_TRNS, KC_TRNS, 
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS,    KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_TRNS, KC_TRNS, 
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_0,       KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_TRNS, KC_TRNS, 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_KP_0,    KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_TRNS, KC_TRNS, 
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
