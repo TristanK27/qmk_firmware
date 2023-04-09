84eb06d666636b66cd202b8042c06c16c4196a54
Tristan Kenis <tristankenis27@gmail.com>
2023-04-10 01:19:36 +0200

    Pro micro compatibility

--- keyboards/splitkb/kyria/keymaps/TristanK27/rules.mk
+++ keyboards/splitkb/kyria/keymaps/TristanK27/rules.mk
@@ -4,3 +4,4 @@ ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 CAPS_WORD_ENABLE = yes
+BOOTLOADER = caterina   # this is the bootloader for the Pro Micro
