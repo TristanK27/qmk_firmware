4492aa179e5c8763c24592448a62c884d786bdfd
TristanK27 <xb9j5e2ff@mozmail.com>
2023-04-09 21:03:12 +0200

    Added numpad layer to kyria.
    Compiles

--- keyboards/splitkb/kyria/keymaps/TristanK27/keymap.c
+++ keyboards/splitkb/kyria/keymaps/TristanK27/keymap.c
@@ -113,6 +113,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 /*
+    * Layer Name: NUMPAD
+    * Description: Numpad Layer
+    *
+    * ,-------------------------------------------.                              ,-------------------------------------------.
+    * |        |      |      |      |      |      |                              |      |  7   |  8   |  9   |      |        |
+    * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+    * |        |      |      |      |      |      |                              |      |  4   |  5   |  6   |      |        |
+    * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+    * |        |      |      |      |      |      |      |      |  |      |      |  0   |  1   |  2   |  3   |      |        |
+    * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+    *                        |      |      |      |      |      |  |      |      |      |      |      |
+    *                        |      |      |      |      |      |  |      |      |      |      |      |
+    *                        `----------------------------------'  `----------------------------------'
+    */
+    [NUMPAD] = LAYOUT(
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                         KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                         KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_TRNS, KC_TRNS,
+                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+/*
  * Layer Name: ADJUST
  * Description: Adjust Layer
  *
