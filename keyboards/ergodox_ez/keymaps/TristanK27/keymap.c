85a6306f7f4a7c8afed08c73b7a5ea218f8f2e56
Tristan Kenis <20356336+TristanK27@users.noreply.github.com>
2023-07-06 13:11:49 +0200

    Added num key

--- keyboards/ergodox_ez/keymaps/TristanK27/keymap.c
+++ keyboards/ergodox_ez/keymaps/TristanK27/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  TRNS  | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |           | TRNS | TRNS | TRNS | TRNS | TRNS | TRNS |  TRNS  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   F15  |  F9  | F10  | F11  | F12  | F18  | TRNS |           | TRNS | TRNS |   7  |   8  |   9  | TRNS | TRNS   |
+ * |   F15  |  F9  | F10  | F11  | F12  | F18  | TRNS |           | TRNS |  NUM |   7  |   8  |   9  | TRNS | TRNS   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |   F14  |  F5  |  F6  |  F7  |  F8  | F17  |------|           |------| TRNS |   4  |   5  |   6  | TRNS | TRNS   |
  * |--------+------+------+------+------+------| TRNS |           | TRNS |------+------+------+------+------+--------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUMB] = LAYOUT_ergodox_pretty(
   KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-  KC_F15, KC_F9, KC_F10,   KC_F11, KC_F12, KC_F18, KC_TRNS,     KC_TRNS, KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
+  KC_F15, KC_F9, KC_F10,   KC_F11, KC_F12, KC_F18, KC_TRNS,     KC_TRNS, KC_NUM,   KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
   KC_F14, KC_F5, KC_F6,  KC_F7, KC_F8, KC_F17,               KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
   KC_F13, KC_F1, KC_F2, KC_F3, KC_F4, KC_F16, KC_TRNS,     KC_TRNS, KC_0, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,
