c2741767088d3ff594a1d2e379aeddd285147ed4
TristanK27 <20356336+TristanK27@users.noreply.github.com>
2026-07-28 21:46:12 +0200

    Add "handwired" dactyl_manuform tristank27

---
+++ devenv.nix
@@ -0,0 +1,7 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [ pkgs.qmk ];
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ keyboards/handwired/dactyl_manuform/5x6_tristank27/config.h
@@ -0,0 +1,2 @@
+// https://docs.qmk.fm/features/split_keyboard#:~:text=Handedness%20by%20EEPROM
+#define EE_HANDS
